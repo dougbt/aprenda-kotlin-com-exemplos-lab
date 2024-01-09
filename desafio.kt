493be6c782a118c0b202b67d112fa3c1b1218f36
Douglas Barcellos <84218639+dougbt@users.noreply.github.com>
2024-01-09 14:11:46 -0300

    desafio completo

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,36 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+enum class Nivel { JUNIOR, PLENO, SENIOR }
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+class Usuario(val name: String, val matricula: String) {
+}
 
-class Usuario
+data class ConteudoEducacional(val nome: String, val duracao: Int)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
-
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
-    
+
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        if (!inscritos.contains(usuario)) {
+            inscritos.add(usuario)
+            println("Aluno: ${usuario.name} - Formacao: ${nome}")
+        } else {
+            println("Aluno já matriculado nesta formação")
+        }
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val usuario1 = Usuario("Eduarda", "123456")
+    val usuario2 = Usuario("Tofu", "987654")
+
+    val conteudo1 = ConteudoEducacional("Orientação a Objetos", 84)
+    val conteudo2 = ConteudoEducacional("Java para Iniciantes", 146)
+
+    val formacao = Formacao("Do Zero ao Junior", listOf(conteudo1, conteudo2))
+
+    formacao.matricular(usuario1)
+    formacao.matricular(usuario2)
+
+    println("\n${formacao.nome}:\n")
+    println(formacao.inscritos.joinToString("\n") { it.name })
 }
